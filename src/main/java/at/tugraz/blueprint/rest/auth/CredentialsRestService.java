e674b174a25354d38789c32da134d24aa4dc3e00
Laura TH <lthaci@tugraz.at>
2024-10-14 15:27:54 +0200

    global: migrate to base version 4.2.0 - improve structure

--- src/main/java/at/tugraz/blueprint/rest/auth/CredentialsRestService.java
+++ src/main/java/at/tugraz/blueprint/rest/auth/CredentialsRestService.java
@@ -2,11 +2,11 @@ package at.tugraz.blueprint.rest.auth;
 
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import org.jboss.resteasy.annotations.jaxrs.FormParam;
 
 @Singleton
 @RegisterRestClient(configKey = "rest.tugraz.api.auth")
--- src/main/java/at/tugraz/damap/rest/projects/TUGrazProjectRestService.java
+++ src/main/java/at/tugraz/damap/rest/projects/TUGrazProjectRestService.java
@@ -1,11 +1,10 @@
 package at.tugraz.damap.rest.projects;
 
 import at.tugraz.blueprint.rest.auth.BearerTokenClientHeadersFactory;
-import at.tugraz.blueprint.rest.services.DBPServiceBase;
-import at.tugraz.damap.rest.dmp.domain.TUGrazProject;
+import at.tugraz.blueprint.rest.services.ProjectServiceBase;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(configKey = "rest.projects")
 @RegisterClientHeaders(BearerTokenClientHeadersFactory.class)
-public interface TUGrazProjectRestService extends DBPServiceBase<TUGrazProject> {}
+public interface TUGrazProjectRestService extends ProjectServiceBase {}
